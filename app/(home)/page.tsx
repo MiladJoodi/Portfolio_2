c86b97a2df8f8780344bcb42a5803097f338abff
Milad Joodi <winbeta.com@gmail.com>
2024-02-25 21:37:49 +0330

    start

---
+++ app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+
+export default function page() {
+  return (
+    <div className="min-h-screen bg-black">
+      <div className="max-w-7xl mx-auto">
+
+        <Navbar />
+
+      </div>
+    </div>
+  );
+}
