e3632a66a3d3fe1faeb829ff2d9743ac5ce85366
Milad Joodi <winbeta.com@gmail.com>
2024-02-26 17:12:12 +0330

    make with ♥

--- app/(home)/components/Footer.tsx
+++ app/(home)/components/Footer.tsx
@@ -5,6 +5,7 @@ export default function Footer() {
   return (
     <div className='border-t mt-10'>
         <Navbar className="flex-col gap-5" />
+        Make with <a href="https://github.com/MiladJoodi/Portfolio_2">❤️</a>
     </div>
   )
 }
